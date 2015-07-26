fb7ee5b1a89b1e07581c9dce8b1824c1c7e952bc
rmjaudenes <rmjaudenes@yahoo.es>
2015-07-26 21:30:39 +0200

    Ejercicio Modulo 8 Añadir temas V2

--- models/quiz.js
+++ models/quiz.js
@@ -21,6 +21,12 @@ module.exports = function(sequelize, DataTypes) {
         type: DataTypes.STRING,
         validate: { notEmpty: {msg: "-> Falta Respuesta"}}
       }
+      
+      ,
+      tema: {
+        type: DataTypes.STRING
+        
+      }
     }
   );
 }
